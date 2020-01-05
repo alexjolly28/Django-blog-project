2c593e7bb29528bec94acafbb8e19df1ef8538d4
alexjolly111 <alexjolly111@gmail.com>
2020-01-05 21:45:06 +0530

    postgressql added

--- django_project/settings.py
+++ django_project/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'alexjolly',
+        'PASSWORD': '1234qwer',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
